ff346ed6a530e35f814c5c60463a67dbac49b2e5
Sergey <kozionov@rambler.ru>
2024-03-21 10:57:19 +0500

    коммит

---
+++ src/main/resources/static/graph.js
@@ -0,0 +1,29 @@
+ function showGraph() {
+ google.charts.load('current', {'packages':['corechart']});
+ google.charts.setOnLoadCallback(drawChart);
+
+    function drawChart() {
+
+if(gr!= null && gr.length>1){
+      var data = google.visualization.arrayToDataTable(gr);
+
+      var view = new google.visualization.DataView(data);
+      view.setColumns([0, 1,
+                       { calc: "stringify",
+                         sourceColumn: 1,
+                         type: "string",
+                         role: "annotation" },
+                       2]);
+
+      var options = {
+        title: "Баллы",
+        width: 600,
+        height: 400,
+        bar: {groupWidth: "95%"},
+        legend: { position: "none" },
+      };
+      var chart = new google.visualization.BarChart(document.getElementById("chart_div"));
+            chart.draw(data, options);
+}
+        }
+        }
