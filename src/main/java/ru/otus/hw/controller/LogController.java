be4a77a9c116ce36a7f52248a4c4e3b1191ab939
Sergey <kozionov@rambler.ru>
2024-03-27 17:34:56 +0500

    коммит

--- src/main/java/ru/otus/hw/controller/LogController.java
+++ src/main/java/ru/otus/hw/controller/LogController.java
@@ -20,27 +20,11 @@ public class LogController {
     public List<LogDto> getAllClubs(@PathVariable("id") long id) {
         return logService.findByClubId(id);
     }
-//
-//    @GetMapping("/api/logs/{id}")
-//    public ClubDto getClub(@PathVariable("id") long id) {
-//        return logService.findById(id);
-//    }
-
-//    @PostMapping("/api/logs")
-//    public ClubDto addRecords(@RequestBody @Valid LogCreateDto logCreateDto) {
-//        logService.insert(logCreateDto);
-//        return new ClubDto();
-//    }
 
     @PutMapping("/api/logs/{id}")
     public ClubDto editClub(@PathVariable("id") long id, @RequestBody @Valid LogCreateDto logCreateDto) {
         logService.insert(id, logCreateDto);
         return new ClubDto();
     }
-//
-//    @DeleteMapping("/api/logs/{id}")
-//    public void deleteClub(@PathVariable("id") long id) {
-//        logService.deleteById(id);
-//    }
 
 }
